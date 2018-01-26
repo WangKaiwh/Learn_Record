1450ff165f02fd75503f65673972518ad3faf4ef
WangKaiwh <15927027211@163.com>
2018-01-26 15:52:10 +0800

    thld.sh insyde sdk中计算阈值的代码优化

--- Bash_shell/project/tothshld/thld.sh
+++ Bash_shell/project/tothshld/thld.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+usage()
+{
+	echo "$0 normalval_decimal"
+	exit
+}
+
+if [ $# != 1 ]; then usage; fi
+
+
 raw=$1
 
-printf "%x\n" $[ $raw  + $raw * 20 / 100 ]
-printf "%x\n" $[ $raw  + $raw * 10 / 100 ]
-printf "%x\n" $[ $raw  + $raw * 5 / 100 ]
-printf "%x\n" $[ $raw  - $raw * 20 / 100 ]
-printf "%x\n" $[ $raw  - $raw * 10 / 100 ]
-printf "%x\n" $[ $raw  - $raw * 5 / 100 ]
+printf "UNR: %x\n" $[ $raw  + $raw * 20 / 100 ]
+printf "UCT: %x\n" $[ $raw  + $raw * 10 / 100 ]
+printf "UNC: %x\n" $[ $raw  + $raw * 5 / 100 ]
+printf "LNR: %x\n" $[ $raw  - $raw * 20 / 100 ]
+printf "LCT: %x\n" $[ $raw  - $raw * 10 / 100 ]
+printf "LNC: %x\n" $[ $raw  - $raw * 5 / 100 ]
 
 
 
